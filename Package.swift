176e61e7a770609b3cf2936dcb27ff589430e11c
fathonyfath <fathony.teguh@gmail.com>
2023-07-20 08:17:25 +0900

    Add Package.swift file

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "CurrencyExchange", 
+    products: [
+        .library(
+            name: "library", 
+            targets: ["library"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "library", 
+            url: "https://github.com/fathonyfath/currency-exchange/releases/download/v1.5.0/library.xcframework.zip",
+            checksum: "272df57e43d43a0752ad766c05a66934"
+        ),
+    ],
+)
